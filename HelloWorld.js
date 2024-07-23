66a3ac3b888afa1dc8a4d7071a88224414d58f1e
arnoriel <azrilluthfimulyadi@gmail.com>
2024-07-23 12:03:25 +0700

    second commit

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,4 +1,9 @@
 var nama = "Azril Lutfhi";
 var usia = 18
+var alamat = "Pasirkoja, Bandung, Jawa Barat. 40232"
 
-console.info("Halo Nama Saya: " + nama, "Usia Saya: " + usia)
+var text = `Halo nama saya ${nama} Usia saya ${usia} alamat saya ${alamat}`;
+
+console.info(text)
+
+console.info("Halo Nama Saya: " + nama, "Usia Saya: " + usia, "Alamat Saya: " + alamat);
